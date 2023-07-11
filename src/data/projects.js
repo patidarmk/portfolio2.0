145835fc72331e10320407af25496c3feeebe571
Mukesh <patidarmukesh123@gmail.com>
2023-07-11 11:09:39 +0530

    initial commit

---
+++ src/data/projects.js
@@ -0,0 +1,63 @@
+// Import images
+// import WebImage1 from '../images/web-project-1.jpg';
+import WebImage2 from '../images/spotify_preview.png';
+// import MobileImage1 from '../images/mobile-project-1.jpg';
+import MobileImage2 from '../images/jobseeker_preview.png';
+import UIImage1 from '../images/crud_preview.png';
+// import UIImage2 from '../images/ui-project-2.jpg';
+
+export const projectsData = [
+	{
+		id: 1,
+		title: 'Spotify React App',
+		category: 'Web Application',
+		img: WebImage2,
+		ProjectHeader: {
+			title: 'Project Management UI - From Context',
+			publishDate: 'Jul 26, 2021',
+			tags: 'UI / Frontend',
+		},
+		code:"",
+		url:"https://patidarmk-spotify.netlify.app/"
+	},
+	{
+		id: 2,
+		title: 'Job-Seeker Profile',
+		category: 'Mobile Application',
+		img: MobileImage2,
+		code:"",
+		url:"https://infosec-task.netlify.app/"
+	},
+	{
+		id: 3,
+		title: 'Crud Using MERN',
+		category: 'UI/UX Design',
+		img: UIImage1,
+		code:"",
+		url:"https://upforcetech-task.netlify.app/"
+	},
+	// {
+	// 	id: 4,
+	// 	title: 'Cloud Storage Platform',
+	// 	category: 'UI/UX Design',
+	// 	img: UIImage2,
+	// 	code:"",
+	// 	url:"https://patidarmk-spotify.netlify.app/",
+	// },
+	// {
+	// 	id: 5,
+	// 	title: 'React Social App',
+	// 	category: 'Mobile Application',
+	// 	img: MobileImage1,
+	// 	code:"",
+	// 	url:"https://patidarmk-spotify.netlify.app/"
+	// },
+	// {
+	// 	id: 6,
+	// 	title: 'Apple Design System',
+	// 	category: 'Web Application',
+	// 	img: WebImage1,
+	// 	code:"",
+	// 	url:"https://patidarmk-spotify.netlify.app/"
+	// }
+];
